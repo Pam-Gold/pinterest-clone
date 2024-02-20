21623c468ea81e55d3e8044fe10274f15c646480
PamGold <badtrailz@gmail.com>
2024-02-20 06:16:09 +0100

    Did a Whole Lot

--- src/components/media-props/ImageDatails.jsx
+++ src/components/media-props/ImageDatails.jsx
@@ -1,7 +1,19 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import {  useParams } from "react-router-dom";
-import { Button, Flex, Grid, Img, Text, Link } from "@chakra-ui/react";
+import { useParams, Link } from "react-router-dom";
+import { Button, Flex, Grid, Img, Text } from "@chakra-ui/react";
+import {
+  EmailShareButton,
+  WhatsappShareButton,
+  PinterestShareButton,
+  TwitterShareButton,
+  EmailIcon,
+  WhatsappIcon,
+  PinterestIcon,
+  XIcon,
+} from "react-share";
+import useToggle from "../../hooks/useToggle";
+
 
 const ImageDetails = () => {
   const { id } = useParams();
@@ -33,6 +45,10 @@ const ImageDetails = () => {
     fetchDetailsData();
   }, []);
 
+
+  const [shareBox, renderShareBox] = useToggle()
+  const [downloadBox, renderDownloadBox] = useToggle()
+
   return (
     <Flex
       as="main"
@@ -53,27 +69,113 @@ const ImageDetails = () => {
         lg: "row",
         xl: "row",
       }}
-
-      
-
       bg="#fff"
     >
-      <Flex  h={{base:"50%", sm:"50%", md:"50%", lg:"100%", xl:"100%"}} w={{base:"100%", sm:"100%", md:"100%", lg:"50%", xl:"50%"}} pos="relative">
+      <Flex
+        h={{ base: "50%", sm: "50%", md: "50%", lg: "100%", xl: "100%" }}
+        w={{ base: "100%", sm: "100%", md: "100%", lg: "50%", xl: "50%" }}
+        pos="relative"
+      >
+        <Img
+          h="100%"
+          w="100%"
+          src={detailData?.urls?.regular}
+          borderRadius={{
+            base: "25px 25px 0px 0px",
+            sm: "25px 25px 0px 0px",
+            md: "25px 25px 0px 0px",
+            lg: "25px 0px 0px 25px",
+            xl: "25px 0px 0px 25px",
+          }}
+        />
 
-      <Img h="100%" w="100%"  src={detailData?.urls?.regular}  borderRadius={{base:"25px 25px 0px 0px", sm:"25px 25px 0px 0px", md:"25px 25px 0px 0px", lg:"25px 0px 0px 25px", xl:"25px 0px 0px 25px"}}/>
-
-      <Button h="40px" w="40px" borderRadius="50%" fontSize="1.4rem" pos="absolute" top="10px" left="10px"><Link to="/"><i className="fa-solid fa-arrow-left"></i></Link>
-      </Button>
-      
+        <Button
+          h="40px"
+          w="40px"
+          borderRadius="50%"
+          fontSize="1.4rem"
+          pos="absolute"
+          top="10px"
+          left="10px"
+        >
+          <Link to="/">
+            <i className="fa-solid fa-arrow-left"></i>
+          </Link>
+        </Button>
       </Flex>
-      <Flex h={{base:"50%", sm:"50%", md:"50%", lg:"100%", xl:"100%"}} w={{base:"100%", sm:"100%", md:"100%", lg:"50%", xl:"50%"}} bg="#f1f1f1" borderRadius={{base:"0px 0px 25px 25px", sm:"0px 0px 25px 25px", md:"0px 25px 25px 0px", lg:"0px 25px 25px 0px"}} p="15px" flexDir="column" pos="relative">
+      <Flex
+        h={{ base: "50%", sm: "50%", md: "50%", lg: "100%", xl: "100%" }}
+        w={{ base: "100%", sm: "100%", md: "100%", lg: "50%", xl: "50%" }}
+        bg="#f1f1f1"
+        borderRadius={{
+          base: "0px 0px 25px 25px",
+          sm: "0px 0px 25px 25px",
+          md: "0px 25px 25px 0px",
+          lg: "0px 25px 25px 0px",
+        }}
+        p="15px"
+        flexDir="column"
+        pos="relative"
+        rowGap="35px"
+      >
         <Flex columnGap="15px" fontSize="1.4rem">
-          <Flex h="40px" w="40px" justify="content" align="center"><i className="fa-solid fa-ellipsis"></i></Flex>
-          <Flex h="40px" w="40px" justify="content" align="center" ><i className="fa-solid fa-arrow-up-from-bracket"></i></Flex>
-          <Flex h="40px" w="40px" justify="content" align="center" ><i className="fa-solid fa-link"></i></Flex>
+          <Flex h="40px" w="40px" justify="content" align="center">
+            <i style={{cursor:"pointer"}} className="fa-solid fa-ellipsis" onClick={renderDownloadBox}></i>
+          </Flex>
+          <Flex h="40px" w="40px" justify="content" align="center">
+            <i style={{cursor:"pointer"}} className="fa-solid fa-arrow-up-from-bracket" onClick={renderShareBox}></i>
+          </Flex>
+          <Flex h="40px" w="40px" justify="content" align="center">
+            <i style={{cursor:"pointer"}} className="fa-solid fa-link" onClick={()=>navigator.clipboard.writeText(detailData?.urls?.regular)} ></i>
+          </Flex>
+        </Flex>
+        <Flex>
+          <Text>{detailData?.alt_description}</Text>
         </Flex>
 
-        <Flex pos="absolute" left="30px" bottom="20px" columnGap="25px"><Text fontWeight="bold">{detailData?.user?.name}</Text> <Link href={detailData?.user?.portfolio_url} target="_blank" ><i className="fa-solid fa-arrow-up-right-from-square"></i></Link></Flex>
+        <Flex
+          pos="absolute"
+          left="20px"
+          bottom="20px"
+          columnGap="25px"
+          align="center"
+        >
+          <Img
+            h="40px"
+            w="40px"
+            borderRadius="50%"
+            src={detailData?.user?.profile_image?.small}
+          />
+          <Text fontWeight="bold">{detailData?.user?.name}</Text>{" "}
+          <a
+            rel="noreferrer"
+            href={detailData?.user?.portfolio_url}
+            target="_blank"
+          >
+            <i className="fa-solid fa-arrow-up-right-from-square"></i>
+          </a>
+        </Flex>
+
+        { shareBox && <Flex top="70px" left="40px" bg="#fff" h="100px" w="fit-content" pos="absolute" borderRadius="15px" columnGap="10px" p="8px">
+          <EmailShareButton  onClick={renderShareBox}   url={detailData?.urls?.regular}>
+            <EmailIcon size={40} round={true}/>
+          </EmailShareButton>
+          <WhatsappShareButton onClick={renderShareBox}    url={detailData?.urls?.regular}>
+          <WhatsappIcon size={40} round={true}/>
+          </WhatsappShareButton>
+          <PinterestShareButton   url={detailData?.urls?.regular}>
+
+<PinterestIcon onClick={renderShareBox}   size={40} round={true}/>
+          </PinterestShareButton>
+          <TwitterShareButton onClick={renderShareBox}   url={detailData?.urls?.regular}>
+            <XIcon size={40} round={true}/>
+          </TwitterShareButton>
+          
+        </Flex>}
+
+        { downloadBox && <Flex top="70px" left="5px" pos="absolute" h="fit-content" w="fit-content" bg="#fff" p="10px 25px" borderRadius="8px">
+          <Text cursor="pointer" onClick={renderDownloadBox}>Download</Text>
+        </Flex>}
       </Flex>
     </Flex>
   );
